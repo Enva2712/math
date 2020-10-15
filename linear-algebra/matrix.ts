bdf8b241c1fd7d345d4654af0025e6f941e02a09
Evan Rusmisel <evanrusmisel@gmail.com>
2020-10-15 14:25:01 -0500

    Rewrite matrix class

--- linear-algebra/matrix.ts
+++ linear-algebra/matrix.ts
@@ -1,164 +1,151 @@
 import Fraction from 'fraction.js';
 
-export type Row = Fraction[];
-export type Dimentions = [number, number];
-export type Cell = [number, number];
-export type HistoryItem = {
-	memo: string,
-	matrixData: Row[],
-}
-
 export default class Matrix {
-	readonly history: HistoryItem[]
-	readonly dimentions: Dimentions
+	readonly cells: Fraction[];
+	readonly width: number;
 
-	static fromArray(numbers: number[][]): Matrix {
-		const height = numbers.length
-		const width = numbers[0].length
-		if(!!numbers.find(row => row.length !== width)) throw new Error('Matrix rows have different lengths')
-
-		return new Matrix([{
-			memo: 'Initialization',
-			matrixData: numbers.map(row => row
-				.map(n => new Fraction(n))
-			)
-		}], [height, width])
+	static fromArray(entries: number[][]): Matrix {
+		const cells = entries.map(r => r.map(c => new Fraction(c))).flat()
+		return new Matrix(cells, entries[0].length)
 	}
 
-	constructor(history: HistoryItem[], dimentions: Dimentions) {
-		this.history = history
-		this.dimentions = dimentions
+	static fromString(string: string): Matrix {
+		const rows = string
+			.split('\n')
+			.filter(l => !!l)
+			.map(line => line.split(','))
+		const cells = rows
+			.flat()
+			.map(cell => new Fraction(cell.trim() || 0))
+		return new Matrix(cells, rows[0].length)
+	}
+
+	static tag(entries: TemplateStringsArray, ...values: unknown[]): Matrix {
+		const string = entries.reduce((acc, str, i) => `${acc}${str}${i < values.length ? values[i] : ''}`, '')
+		return Matrix.fromString(string)
 	}
 
 	toString(): string {
-		return historyItemToString(this.history[0])
-	}
-
-	toArray(): number[][] {
-		return this.history[0].matrixData.slice()
-			.map(row =>
-				row.map(f => f.valueOf())
+		const rows = [...this.rows()]
+			.map(r => r
+				.map(c => c.toString())
 			)
+		const longestEntry = rows
+			.flat()
+			.reduce((acc, cur) => Math.max(acc, cur.length), 0)
+		return rows
+			.map(r => r
+				.map(c => c.padStart(longestEntry + 1))
+				.join(', ')
+			)
+			.join('\n')
 	}
 
-	definedAt([row, col]: Cell): boolean {
-		const [height, width] = this.dimentions
-		return (
-			1 <= row && row <= height &&
-			1 <= col && col <= width
-		)
+	constructor(cells: Fraction[], width: number) {
+		this.cells = cells;
+		this.width = width;
 	}
 
-	row(i: number): Row {
-		return this.history[0].matrixData[i - 1]
+	clone(): Matrix {
+		const clonedCells = this.cells.map(f => f.clone())
+		return new Matrix(clonedCells, this.width)
 	}
 
-	valueAt(cell: Cell): Fraction | undefined {
-		if(!this.definedAt(cell)) return undefined
-		const [row, col] = cell
-		return this.history[0].matrixData[row-1][col-1]
+	getIndex(row: number, column: number): number {
+		if(this.width < column || this.cells.length/this.width < row || row < 1 || column < 1) throw new Error('Index out of range')
+		return (row-1) * this.width + (column-1);
 	}
 
-	toHistoryString(showMemo = true): string {
-		return this.history
-			.reverse()
-			.map(i => historyItemToString(i, showMemo))
-			.join('\n\n')
+	isSquare(): boolean {
+		return this.cells.length === (this.width ** 2)
 	}
 
-	scaleRow(row: number, weight: number): Matrix {
-		if(1 > row || row > this.dimentions[0]) throw new Error(`Row ${row} is outside of the matrix`);
+	withoutRow(row: number): Matrix {
+		const newMatrix = this.clone()
+		newMatrix.cells.splice(newMatrix.getIndex(row, 1), newMatrix.width)
+		return newMatrix;
+	}
 
-		const newMatrixData = this.history[0].matrixData.slice()
-		newMatrixData[row-1] = newMatrixData[row-1].map(entry => new Fraction(weight).mul(entry))
-		const newHistoryItem: HistoryItem = {
-			memo: `Row ${row} scaled by ${weight}`,
-			matrixData: newMatrixData
+	withoutCol(col: number): Matrix {
+		if(col < 1 || col > this.width) throw new Error('Column out of range')
+		const newCells = this.cells
+			.map(c => c.clone())
+			// Filter out all entries from the provided column
+			.filter((_, i) => (i % this.width - col + 1 === 0))
+		return new Matrix(newCells, this.width - 1);
+	}
+
+	row(i: number): Fraction[] {
+		return this.cells
+			.slice(this.getIndex(i, 1), 1 + this.getIndex(i, this.width))
+			.map(c => c.clone());
+	}
+
+	column(c: number): Fraction[] {
+		const col = [];
+		for(let r = 1; r <= this.cells.length/this.width; r++) {
+			col.push(this.cells[this.getIndex(r, c)].clone())
 		}
-		return new Matrix([newHistoryItem, ...this.history], this.dimentions);
+		return col;
 	}
 
-	swapRows(r1: number, r2: number): Matrix {
-		if(1 > r1 || r1 > this.dimentions[0]) throw new Error(`Row ${r1} is outside of the matrix`);
-		if(1 > r2 || r2 > this.dimentions[0]) throw new Error(`Row ${r2} is outside of the matrix`);
-
-		const newMatrixData: Row[] = this.history[0].matrixData.slice()
-		const [r1Data, r2Data] = [newMatrixData[r1-1], newMatrixData[r2-1]]
-		newMatrixData[r1-1] = r2Data;
-		newMatrixData[r2-1] = r1Data;
-		const newHistoryItem: HistoryItem = {
-			memo: `Rows ${r1} and ${r2} swaped`,
-			matrixData: newMatrixData
-		}
-		return new Matrix([newHistoryItem, ...this.history], this.dimentions);
-	}
-
-	addRows(r1: number, r2: number, scale = 1): Matrix {
-		if(1 > r1 || r1 > this.dimentions[0]) throw new Error(`Row ${r1} is outside of the matrix`);
-		if(1 > r2 || r2 > this.dimentions[0]) throw new Error(`Row ${r2} is outside of the matrix`);
-
-		const newMatrixData: Row[] = this.history[0].matrixData.slice()
-		newMatrixData[r2-1] = newMatrixData[r2-1]
-			.map((v, i) => newMatrixData[r1-1][i].mul(scale).add(v))
-		const memo = (scale !== 1)
-			? `${scale} times row ${r1} added to row ${r2}`
-			: `Row ${r1} added to row ${r1}`
-		const newHistoryItem: HistoryItem = {
-			memo,
-			matrixData: newMatrixData
-		}
-		return new Matrix([newHistoryItem, ...this.history], this.dimentions);
-	}
-
-	//TODO
-	isInRowEchelon(): boolean {
-		return false
-	}
-
-	//TODO
-	applyGaussianElimination(): Matrix {
-		return toRowEchelon(this)
-	}
-}
-
-const findMax = (a: number, item: number) => a < item ? item : a
-
-function historyItemToString({ matrixData, memo }: HistoryItem, showMemo: boolean = false): string {
-	let str = showMemo ? memo + '\n' : ''
-	let width = 1 + matrixData
-		.map(row => row
-			.map(i => i.toFraction().length)
-			.reduce(findMax)
-		)
-		.reduce(findMax)
-	str += matrixData
-		.map(row => `|${row.map(n => n.toFraction().padStart(width)).join('')} |`)
-		.join('\n')
-	return str
-}
-
-const findPivot = (m: Matrix, skip: number): Cell | undefined => {
-	const [height, width] = m.dimentions
-	for(let col = 1; col < width; col++) {
-		for(let row = skip + 1; row < height; row++) {
-			const c: Cell = [row, col]
-			if(m.valueAt(c)) return c
+	*rows(): Generator<Fraction[]> {
+		for (let i = 0; i < this.cells.length/this.width; i++) {
+			yield this.row(i+1)
 		}
 	}
-	return undefined
-}
 
-//TODO
-function toRowEchelon(m: Matrix, skip: number = 0): Matrix {
-	if(m.isInRowEchelon()) return m
-	let pivot = findPivot(m, skip)
-	console.log(pivot)
-	return m
-}
+	*columns(): Generator<Fraction[]> {
+		for(let i = 0; i < this.width; i++) {
+			yield this.column(i+1)
+		}
+	}
 
-//TODO
-/*
-function reduceRowEchelon(m: Matrix, skip: number = 0): Matrix {
-	return m
+	static add(m1: Matrix, m2: number | Fraction | Matrix): Matrix {
+		if(typeof m2 === 'number') m2 = new Fraction(m2)
+		if(m2 instanceof Fraction) {
+			return new Matrix(m1.cells.map(c => c.add(m2 as Fraction)), m1.width)
+		}
+		if(m1.width !== m2.width || m1.cells.length !== m2.cells.length) throw new Error('Matrices must be the same dimentions to be added');
+		const newCells = m1.cells.map((c, i) => (c.add((m2 as Matrix).cells[i])));
+		return new Matrix(newCells, m1.width)
+	}
+
+	static sub(m1: Matrix, m2: Matrix | Fraction | number): Matrix {
+		if(typeof m2 === 'number') m2 = new Fraction(m2)
+		if(m2 instanceof Fraction) return new Matrix(m1.cells.map(c => c.add(m2 as Fraction)), m1.width)
+		if(m1.width !== m2.width || m1.cells.length !== m2.cells.length) throw new Error('Matrices must be the same dimentions to be subtracted');
+		const newCells = m1.cells.map((c, i) => (c.sub((m2 as Matrix).cells[i])));
+		return new Matrix(newCells, m1.width)
+	}
+
+	static mul(m1: Matrix, m2: number | Fraction | Matrix): Matrix {
+		// TODO
+		return m1.add(m2);
+	}
+
+	static det(m: Matrix): Fraction {
+		if(!m.isSquare()) throw new Error('A matrix must be square to compute its determinant');
+
+		if(m.width < 1) return new Fraction(1)
+		else if(m.width === 1) return m.cells[0];
+		else if(m.width === 2) {
+			return m.cells[0].mul(m.cells[3]).add(m.cells[1].mul(m.cells[2]))
+		}
+		else {
+			const firstRow = m.row(1)
+			return firstRow
+				.map((c, i) => {
+					const sign = (i+1)%2*2-1
+					const subMatrix = m.withoutRow(1).withoutCol(i + 1)
+					return subMatrix.det().mul(c).mul(sign)
+				})
+				.reduce((acc, val) => acc.add(val), new Fraction(1))
+		}
+	}
+
+	add(m: number | Fraction | Matrix): Matrix { return Matrix.add(this, m); }
+	sub(m: number | Fraction | Matrix): Matrix { return Matrix.sub(this, m); }
+	mul(m: number | Fraction | Matrix): Matrix { return Matrix.mul(this, m); }
+	det(): Fraction { return Matrix.det(this) }
 }
-*/
