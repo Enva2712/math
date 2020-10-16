d8388a08191ac4cc89200ccdc1193e8a513b9351
Evan Rusmisel <evanrusmisel@gmail.com>
2020-10-16 17:20:33 -0500

    Format code

--- rollup.config.js
+++ rollup.config.js
@@ -1,26 +1,23 @@
-import typescript from '@rollup/plugin-typescript';
+import typescript from "@rollup/plugin-typescript";
 import { terser } from "rollup-plugin-terser";
 
 export default {
-    input: 'src/index.ts',
-    plugins: [
-        typescript(),
-        terser(),
-    ],
-    external: ['fraction.js', 'tslib'],
+    input: "src/index.ts",
+    plugins: [typescript(), terser()],
+    external: ["fraction.js", "tslib"],
     output: [
         {
-            file: 'dist/index.js',
-            format: 'umd',
-            name: 'envaMath',
-            exports: 'named',
+            file: "dist/index.js",
+            format: "umd",
+            name: "envaMath",
+            exports: "named",
             sourcemap: true,
         },
         {
-            file: 'dist/index.es.js',
-            format: 'esm',
-            exports: 'named',
+            file: "dist/index.es.js",
+            format: "esm",
+            exports: "named",
             sourcemap: true,
         },
     ],
-}
+};
