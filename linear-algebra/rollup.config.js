65a4a35923974b4ea53be52e5048301084c13648
Evan Rusmisel <evanrusmisel@gmail.com>
2020-10-16 17:17:56 -0500

    Add terser

--- linear-algebra/rollup.config.js
+++ linear-algebra/rollup.config.js
@@ -1,15 +1,18 @@
 import typescript from '@rollup/plugin-typescript';
+import { terser } from "rollup-plugin-terser";
 
 export default {
     input: 'src/index.ts',
     plugins: [
         typescript(),
+        terser(),
     ],
     external: ['fraction.js', 'tslib'],
     output: [
         {
             file: 'dist/index.js',
-            format: 'cjs',
+            format: 'umd',
+            name: 'envaMath',
             exports: 'named',
             sourcemap: true,
         },
